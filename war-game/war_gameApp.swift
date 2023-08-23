1680e050e8c8fa9cd9c69599eb7e6cd4fc4c7978
Debajyoti Saha <debajyotisaha14@gmail.com>
2023-08-23 19:51:21 +0530

    Initial Commit

---
+++ war-game/war_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_gameApp.swift
+//  war-game
+//
+//  Created by Debajyoti Saha on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct war_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
